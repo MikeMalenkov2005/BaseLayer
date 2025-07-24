6124fdecd2347da3817a50ced970acd5294d7070
MikeMalenkov2005 <mike.malenkov@gmail.com>
2025-07-24 20:35:28 +0300

    Fixed ArenaAllocate and started working on Heap.

--- include/mem.h
+++ include/mem.h
@@ -12,10 +12,7 @@ c_linkage_begin
 #define MEM_FastAlignUp(x, n) MEM_FastAlignDown((x)+(n)-1, n)
 
 #define MEM_ARENA_DEFAULT_SIZE  GiB(1)
-
-#ifndef MEM_ARENA_COMMIT_SIZE
 #define MEM_ARENA_COMMIT_SIZE   KiB(8)
-#endif
 
 typedef struct MEM_Arena
 {
@@ -48,6 +45,31 @@ typedef struct MEM_ArenaLevel
 
 MEM_ArenaLevel MEM_ArenaLevelOpen(MEM_Arena *arena);
 void MEM_ArenaLevelClosee(MEM_ArenaLevel level);
+
+#define MEM_HEAP_DEFAULT_SIZE GiB(1)
+#define MEM_HEAP_COMMIT_SIZE  KiB(8)
+
+typedef struct MEM_Heap
+{
+  U8 *memory;
+  UZ size;
+  UZ commited;
+  UZ head;
+} MEM_Heap;
+
+MEM_Heap MEM_HeapInit(UZ size);
+void MEM_HeapClear(MEM_Heap *heap);
+void MEM_HeapFree(MEM_Heap *heap);
+
+void *MEM_HeapAllocate(MEM_Heap *heap, UZ size);
+void *MEM_HeapAllocateZero(MEM_Heap *heap, UZ size);
+void *MEM_HeapReallocate(MEM_Heap *heap, void *memory, UZ size);
+void MEM_HeapDeallocate(MEM_Heap *heap, void *memory);
+
+#define MEM_HeapAllocateArraySized(heap, count, size) \
+  MEM_HeapAllocateZero(arena, (count) * (size))
+#define MEM_HeapAllocateArrayTyped(heap, count, type) \
+  MEM_HeapAllocateArraySized(arena, count, sizeof(type))
 
 c_linkage_end
 
