631dc908f6bdf54f25fbb28b098014e252774f73
MikeMalenkov2005 <mike.malenkov@gmail.com>
2025-08-21 15:19:22 +0300

    Added DS_Vector generic structure.

--- include/ds.h
+++ include/ds.h
@@ -37,10 +37,59 @@ typedef struct DS_Array(T)  \
   UZ size;                  \
 } DS_Array(T)
 
-#define DS_ArrayAllocate(T, mem, size) \
-  ((DS_Array(T)) { MEM_AllocateArrayTyped(mem, size, T), (size) })
+#define DS_ArrayInit(T, data, size) ((DS_Array(T)) { (data), (size) })
+#define DS_ArrayAllocate(T, mem, size) DS_ArrayInit(T, MEM_AllocateArrayTyped(mem, size, T), size)
+#define DS_ArrayDeallocate(mem, arr) MEM_Deallocate(mem, (arr).data)
 
 #define X DS_ArrayDefine
+X_FOR_BASE_TYPES
+#undef X
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+*                                    VECTOR                                    *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#define DS_Vector(T) Glue(DS_Vector_, T)
+
+#define DS_VectorDefine(T)  \
+typedef struct DS_Vector(T) \
+{                           \
+  T *data;                  \
+  UZ size;                  \
+  UZ capacity;              \
+} DS_Vector(T)
+
+#define DS_VectorInit() ((DS_Vector(T)) { null })
+
+#define DS_VectorResize(mem, vec, sz) Statement(                    \
+  void *__data = (vec)->data;                                       \
+  UZ __cap = (vec)->capacity;                                       \
+  __cap = MEM_FastAlignDown(__cap, 8);                              \
+  while ((sz) > __cap) __cap += 8;                                  \
+  if (__cap > (vec)->capacity)                                      \
+  {                                                                 \
+    __data = MEM_Reallocate(mem, __data, __cap * sizeof(*__data));  \
+    if (!__data) break; /* TODO: hadnle errors later! */            \
+  }                                                                 \
+  (vec)->data = __data;                                             \
+  (vec)->size = (sz);                                               \
+  (vec)->capacity = __cap;                                          \
+)
+
+#define DS_VectorAppend(mem, vec, val) Statement(             \
+  UZ __size = (vec)->size + 1;                                \
+  DS_VectorResize(mem, vec, __size);                          \
+  if (__size == (vec)->size) (vec)->data[__size - 1] = (val); \
+)
+
+#define DS_VectorClear(mem, vec) Statement(           \
+  if ((vec)->data) MEM_Deallocate(mem, (vec)->data);  \
+  (vec)->data = nullptr;                              \
+  (vec)->size = 0;                                    \
+  (vec)->capacity = 0;                                \
+)
+
+#define X DS_VectorDefine
 X_FOR_BASE_TYPES
 #undef X
 
